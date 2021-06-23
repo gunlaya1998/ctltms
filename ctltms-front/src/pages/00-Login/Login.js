ebcb604c3335d086e9ab748babc41654d753cb38
LAPTOP-05NRR3PN\kanla <gunlaya1998@hotmail.com>
2021-06-23 23:28:23 +0700

    [+] Login

--- ctltms-front/src/pages/00-Login/Login.js
+++ ctltms-front/src/pages/00-Login/Login.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom';
-import axios from 'axios'
+import axios from 'axios';
+import styled from 'styled-components';
 import TextField from '@material-ui/core/TextField';
-import './Login.css'
+import './Login.css';
+import truck from '../../images/05-truck.png';
+
+const StyledTextfield = styled(TextField)`
+    width: 15vw;
+    /* margin-bottom: 25px; */
+    && .MuiFormControl-root {
+        margin: 10px 0 5px 0;;
+    }
+    && .MuiFormLabel-root {
+        color: #5c5c5c;
+        font-family: 'Kanit';
+        font-size: 14px;
+        font-weight: 300;
+    }
+`
 
 export default class main extends Component {
     state = {
-        // usernameReq: "",
-        // passwordReq: "",
         username: "",
         password: "",
         loginStatus: "",
@@ -32,34 +46,38 @@ export default class main extends Component {
     
     render(){
         return (
-            <div className="bg">
-                <div className="login-container">
-                    <h1 className="flex">Log In</h1>
-                    <TextField
-                        className="textfield"
-                        placeholder="username"
-                        type="text"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={(e) => {
-                            this.setState({username: e.target.value});
-                        }}
-                    /> 
-                    <TextField
-                        className="textfield"
-                        placeholder="password"
-                        type="password"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={(e) => {
-                            this.setState({password: e.target.value});
-                        }}
-                    /> 
-                    <p className="red"> {this.state.loginStatus} </p>   
-                    <button onClick={this.login}>Log In</button> 
-                    {this.state.isLoggedIn ? <Redirect to="/home" />
-                        :null
-                    }
+            <div className="background">
+                <div className="content-container">
+                    <div className="data-container">
+                        <div className="textImg-container">
+                            <img src={truck} width="90%" />
+                        </div>
+                        <div className="textImg-container">
+                            <div className="login-title">เข้าสู่ระบบ</div>
+                            <StyledTextfield 
+                                id="standard-basic" 
+                                label="ชื่อบัญชีผู้ใช้งาน" 
+                                onChange={(e) => {
+                                    this.setState({username: e.target.value});
+                                }}
+                            />
+                            <StyledTextfield 
+                                id="standard-basic" 
+                                label="รหัสผ่าน" 
+                                onChange={(e) => {
+                                    this.setState({password: e.target.value});
+                                }}
+                            />
+
+                            <p className="error-message"> {this.state.loginStatus} </p>
+                            <p className="forgotPassword">ลืมรหัสผ่าน</p>
+                            
+                            <button onClick={this.login} className="login-button">เข้าสู่ระบบ</button> 
+                            {this.state.isLoggedIn ? <Redirect to="/home" />
+                                :null
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         )
